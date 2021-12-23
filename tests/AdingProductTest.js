e8e0d082c758621d4e696898b0278e567591cbbb
NilanjanaKloud9 <nilanjana.d@kloud9.nyc>
2021-12-23 19:07:48 +0530

    commit2

--- tests/AdingProductTest.js
+++ tests/AdingProductTest.js
@@ -8,12 +8,12 @@ import CartPage from '../pages/CartPage';
 const dataSet = require('../data/data.json');
 //import LogOutpage from '../pages/LogOutpage';
 
-//const URL = 'https://www.amazon.com/';
+const URL = 'https://www.amazon.com/';
 //const getURL = ClientFunction(() => window.location.href);
-//fixture ("OrderProduct")
-   // .page (URL);
-    fixture `OrderProduct`
-    .page `http://localhost:8080`;
+fixture ("OrderProduct")
+    .page (URL);
+    /*fixture `OrderProduct`
+    .page `http://localhost:8080`;*/
 dataSet.forEach( data => {
 test('Adding Product Test', async t => {
     await t
